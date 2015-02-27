100f15460d711b52c8d3d216fbab7939ec280bd8
Raphael Seher <raphael@xamoom.com>
2015-02-27 11:22:46 +0100

    Cleaned Core Data files

--- src/coredata/XMMCoreDataGetByLocationIdentifier.h
+++
@@ -1,9 +0,0 @@
-#import "_XMMCoreDataGetByLocationIdentifier.h"
-
-@interface XMMCoreDataGetByLocationIdentifier : _XMMCoreDataGetByLocationIdentifier {}
-
-+ (NSDictionary *)getMapping;
-
-- (NSArray *)sortedMenuItem;
-
-@end
