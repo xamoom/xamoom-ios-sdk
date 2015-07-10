567c7c5f16616a3dc901ac8204f7a8afed14347e
Raphael Seher <raphael@xamoom.com>
2015-07-10 12:58:22 +0200

    Replaced delegate with completionHandlers

---
+++ src/mapping/XMMError.m
@@ -0,0 +1,13 @@
+//
+//  XMMError.m
+//  xamoom-ios-sdk-test
+//
+//  Created by Raphael Seher on 10.07.15.
+//  Copyright (c) 2015 Raphael Seher. All rights reserved.
+//
+
+#import "XMMError.h"
+
+@implementation XMMError
+
+@end
