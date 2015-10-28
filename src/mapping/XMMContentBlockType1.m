bf040af9038288c12c27359db4943191ad3b1b6b
Raphael Seher <raphael@xamoom.com>
2015-10-28 09:52:57 +0100

    Add XMMTableViewRepresentation

--- src/mapping/XMMContentBlockType1.m
+++ src/mapping/XMMContentBlockType1.m
@@ -21,7 +21,6 @@
 
 @implementation XMMContentBlockType1
 
-
 + (RKObjectMapping *)mapping {
   RKObjectMapping* mapping = [RKObjectMapping mappingForClass:[XMMContentBlockType1 class] ];
   [mapping addAttributeMappingsFromDictionary:@{@"file_id":@"fileId",
@@ -38,6 +37,36 @@
                                                                  expectedValue:@"1"
                                                                  objectMapping:[self mapping]];
   return matcher;
+}
+
+#pragma mark - XMMTableViewRepresentation
+
+-(UITableViewCell *)tableView:(UITableView *)tableView representationAsCellForRowAtIndexPath:(NSIndexPath *)indexPath {
+  XMMContentBlock1TableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"AudioBlockTableViewCell"];
+  if (cell == nil) {
+    [tableView registerNib:[UINib nibWithNibName:@"XMMContentBlock1TableViewCell" bundle:nil]
+    forCellReuseIdentifier:@"AudioBlockTableViewCell"];
+    cell = [tableView dequeueReusableCellWithIdentifier:@"AudioBlockTableViewCell"];
+  }
+  
+  //set audioPlayerControl delegate and initialize
+  cell.audioPlayerControl.delegate = cell;
+  [cell.audioPlayerControl initAudioPlayerWithUrlString:self.fileId];
+  
+  //set title & artist
+  if(self.title != nil && ![self.title isEqualToString:@""]) {
+    cell.titleLabel.text = self.title;
+  }
+  
+  if(self.artist != nil && ![self.artist isEqualToString:@""]) {
+    cell.artistLabel.text = self.artist;
+  }
+  
+  //set songDuration
+  float songDurationInSeconds = CMTimeGetSeconds(cell.audioPlayerControl.audioPlayer.currentItem.asset.duration);
+  cell.remainingTimeLabel.text = [NSString stringWithFormat:@"%d:%02d", (int)songDurationInSeconds / 60, (int)songDurationInSeconds % 60];
+  
+  return cell;
 }
 
 @end
