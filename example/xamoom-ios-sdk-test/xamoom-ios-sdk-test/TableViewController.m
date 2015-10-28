52191440061f6e0c63a2cbcfe9c79bcf023f0d89
Raphael Seher <raphael@xamoom.com>
2015-10-28 15:46:16 +0100

    Load TableViewController from storyboard

--- example/xamoom-ios-sdk-test/xamoom-ios-sdk-test/TableViewController.m
+++ example/xamoom-ios-sdk-test/xamoom-ios-sdk-test/TableViewController.m
@@ -20,6 +20,7 @@
   [super viewDidLoad];
   // Do any additional setup after loading the view.
   
+  
   [self.tableView setSeparatorStyle:UITableViewCellSeparatorStyleNone];
   self.tableView.rowHeight = UITableViewAutomaticDimension;
   self.tableView.estimatedRowHeight = 150.0;
@@ -58,10 +59,12 @@
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
   [tableView deselectRowAtIndexPath:indexPath animated:NO];
-  if ([(self.contentBlocks.itemsToDisplay)[indexPath.row] isKindOfClass:[XMMContentBlock6TableViewCell class]]) {
-    XMMContentBlock6TableViewCell *cell = (self.contentBlocks.itemsToDisplay)[indexPath.row];
+  if ([[tableView cellForRowAtIndexPath:indexPath] isKindOfClass:[XMMContentBlock6TableViewCell class]]) {
+    XMMContentBlock6TableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
     
-    TableViewController *vc = [[TableViewController alloc] init];
+    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:[NSBundle mainBundle]];
+    TableViewController *vc = [storyboard  instantiateViewControllerWithIdentifier:@"TableViewController"];
+
     [vc setContentId:cell.contentId];
     [self.navigationController pushViewController:vc animated:YES];
   }
