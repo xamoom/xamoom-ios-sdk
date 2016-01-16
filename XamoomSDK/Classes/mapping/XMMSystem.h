a280930f05d504c0e97b15c20be6aedf17f74790
Raphael Seher <raphael@xamoom.com>
2016-01-16 08:41:03 +0100

    Add XMMSystem

---
+++ XamoomSDK/Classes/mapping/XMMSystem.h
@@ -0,0 +1,13 @@
+//
+//  XMMSystem.h
+//  XamoomSDK
+//
+//  Created by Raphael Seher on 16/01/16.
+//  Copyright © 2016 xamoom GmbH. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XMMSystem : NSObject
+
+@end
---
+++ XamoomSDK/Classes/mapping/XMMSystem.m
@@ -0,0 +1,13 @@
+//
+//  XMMSystem.m
+//  XamoomSDK
+//
+//  Created by Raphael Seher on 16/01/16.
+//  Copyright © 2016 xamoom GmbH. All rights reserved.
+//
+
+#import "XMMSystem.h"
+
+@implementation XMMSystem
+
+@end
