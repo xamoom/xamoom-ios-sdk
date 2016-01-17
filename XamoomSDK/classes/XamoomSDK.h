ab401b01a7e87294881ed446903a5fcd33552973
Raphael Seher <raphael.seher@gmail.com>
2016-01-17 11:21:31 +0100

    Add XMMContentBlocks to framwork header file

--- XamoomSDK/classes/XamoomSDK.h
+++ XamoomSDK/classes/XamoomSDK.h
@@ -17,3 +17,4 @@ FOUNDATION_EXPORT const unsigned char XamoomSDKVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <XamoomSDK/PublicHeader.h>
 
 #import <XamoomSDK/XMMEnduserApi.h>
+#import <XamoomSDK/XMMContentBlocks.h>
