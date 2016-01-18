0f339a4e0014e9d1c09f325bb61913e342e45e22
Raphael Seher <raphael@xamoom.com>
2016-01-18 15:40:31 +0100

    Add tests for XMMRestClient

--- XamoomSDKTests/XMMRestClientTest.m
+++ XamoomSDKTests/XMMRestClientTest.m
@@ -7,33 +7,101 @@
 //
 
 #import <XCTest/XCTest.h>
+#import <OCMock/OCMock.h>
+#import "XMMRestClient.h"
+#import "XMMSystem.h"
 
 @interface XMMRestClientTest : XCTestCase
+
+@property NSString *devApiUrl;
 
 @end
 
 @implementation XMMRestClientTest
 
 - (void)setUp {
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+  [super setUp];
+  self.devApiUrl = @"https://22-dot-xamoom-api-dot-xamoom-cloud-dev.appspot.com/_api/v2/consumer";
 }
 
 - (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
+  [super tearDown];
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
+- (void)testInitWithBaseUrl {
+  XMMRestClient *restClient = [[XMMRestClient alloc] initWithBaseUrl:[NSURL URLWithString:self.devApiUrl] session:[NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration]]];
+  XCTAssertNotNil(restClient);
+  XCTAssertNotNil(restClient.query);
+  XCTAssertNotNil(restClient.session);
 }
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
+- (void)testFetchResourceCallsCallback {
+  XCTestExpectation *expectation = [self expectationWithDescription:@"Handler called"];
+  id session = OCMPartialMock([NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration]]);
+  XMMRestClient *restClient = [[XMMRestClient alloc] initWithBaseUrl:[NSURL URLWithString:self.devApiUrl]
+                                                             session:session];
+  
+  void (^proxyBlock)(NSInvocation *) = ^(NSInvocation *invocation) {
+    void (^passedBlock)(NSData *data, NSURLResponse *response, NSError *error);
+    [invocation getArgument: &passedBlock atIndex: 3];
+    passedBlock([NSData new], nil, nil);
+  };
+  
+  [[[session stub] andDo:proxyBlock] dataTaskWithURL:[OCMArg any] completionHandler:[OCMArg any]];
+  
+  [restClient fetchResource:[XMMSystem class] completion:^(JSONAPI *result, NSError *error) {
+    XCTAssertNotNil(result);
+    XCTAssertNil(error);
+    [expectation fulfill];
+  }];
+  
+  [self waitForExpectationsWithTimeout:1.0 handler:nil];
+}
+
+- (void)testFetchResourceSessionIdCallsCallback {
+  XCTestExpectation *expectation = [self expectationWithDescription:@"Handler called"];
+  id session = OCMPartialMock([NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration]]);
+  XMMRestClient *restClient = [[XMMRestClient alloc] initWithBaseUrl:[NSURL URLWithString:self.devApiUrl]
+                                                             session:session];
+  
+  void (^proxyBlock)(NSInvocation *) = ^(NSInvocation *invocation) {
+    void (^passedBlock)(NSData *data, NSURLResponse *response, NSError *error);
+    [invocation getArgument: &passedBlock atIndex: 3];
+    passedBlock([NSData new], nil, nil);
+  };
+  
+  [[[session stub] andDo:proxyBlock] dataTaskWithURL:[OCMArg any] completionHandler:[OCMArg any]];
+  
+  [restClient fetchResource:[XMMSystem class] id:@"1234" parameters:nil completion:^(JSONAPI *result, NSError *error) {
+    XCTAssertNotNil(result);
+    XCTAssertNil(error);
+    [expectation fulfill];
+  }];
+  
+  [self waitForExpectationsWithTimeout:1.0 handler:nil];
+}
+
+- (void)testFetchResourceSessionIdCallsCallbackError {
+  XCTestExpectation *expectation = [self expectationWithDescription:@"Handler called"];
+  id session = OCMPartialMock([NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration]]);
+  XMMRestClient *restClient = [[XMMRestClient alloc] initWithBaseUrl:[NSURL URLWithString:self.devApiUrl]
+                                                             session:session];
+  
+  void (^proxyBlock)(NSInvocation *) = ^(NSInvocation *invocation) {
+    void (^passedBlock)(NSData *data, NSURLResponse *response, NSError *error);
+    [invocation getArgument: &passedBlock atIndex: 3];
+    passedBlock([NSData new], nil, [[NSError alloc] initWithDomain:@"com.xamoom.unittest" code:1 userInfo:nil]);
+  };
+  
+  [[[session stub] andDo:proxyBlock] dataTaskWithURL:[OCMArg any] completionHandler:[OCMArg any]];
+  
+  [restClient fetchResource:[XMMSystem class] id:@"1234" parameters:nil completion:^(JSONAPI *result, NSError *error) {
+    XCTAssertNotNil(error);
+    XCTAssertNil(result);
+    [expectation fulfill];
+  }];
+  
+  [self waitForExpectationsWithTimeout:1.0 handler:nil];
 }
 
 @end
