89a5a4d57c8f962f9478cef7f0ce9962340aaf71
Raphael Seher <raphael@xamoom.com>
2017-01-24 12:04:51 +0100

    Add comments

--- XamoomSDK/Classes/Storage/XMMOfflineFileManager.h
+++ XamoomSDK/Classes/Storage/XMMOfflineFileManager.h
@@ -13,14 +13,46 @@
 
 extern NSString *const kXamoomOfflineSaveFileFromUrlError;
 
+/**
+ * Get the url to the localfile from urlString.
+ *
+ * @param urlString Url string from xamoom cloud.
+ * @return NSURL to saved file.
+ */
 - (NSURL *)urlForSavedData:(NSString *)urlString;
 
+/**
+ * Download and save file from url.
+ *
+ * @param urlString Url string from xamoom cloud.
+ * @param completion Block for completion
+ */
 - (void)saveFileFromUrl:(NSString *)urlString completion:(void(^)(NSString *url, NSData *data, NSError *error))completion;
 
+/**
+ * Get data from saved file.
+ *
+ * @param urlString Url string from xamoom cloud.
+ * @param error Error reference.
+ * @return NSData from file, nil if error.
+ */
 - (NSData *)savedDataFromUrl:(NSString *)urlString error:(NSError **)error;
 
+/**
+ * Get image from saved file.
+ *
+ * @param urlString Url string from xamoom cloud.
+ * @param error Error reference.
+ * @return UIImage from file, nil if error.
+ */
 - (UIImage *)savedImageFromUrl:(NSString *)urlString error:(NSError **)error;
 
+/**
+ * Delete locally saved file.
+ *
+ * @param urlString Url string from xamoom cloud.
+ * @param error Error reference.
+ */
 - (void)deleteFileWithUrl:(NSString *)urlString error:(NSError **)error;
 
 @end
