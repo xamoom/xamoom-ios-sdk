dfd28eda636b53f345ac3f684d7b40ef7716e9b3
Raphael Seher <raphael@xamoom.com>
2017-07-03 14:13:45 +0200

    Raise version to 3.3.0

--- XamoomSDKTests/XMMEnduserApiTest.m
+++ XamoomSDKTests/XMMEnduserApiTest.m
@@ -54,7 +54,7 @@
 
 - (void)testInitWithApiKey {
   NSDictionary *httpHeaders = @{@"Content-Type":@"application/vnd.api+json",
-                                @"User-Agent":@"XamoomSDK iOS|(null)|3.2.2",
+                                @"User-Agent":@"XamoomSDK iOS|(null)|3.3.0",
                                 @"APIKEY":@"apikey",};
   
   XMMEnduserApi *customApi = [[XMMEnduserApi alloc] initWithApiKey:@"apikey"];
