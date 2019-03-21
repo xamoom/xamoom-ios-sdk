60baded00e04bef7609b7cbab15872c7251e3394
Thomas Krainz-Mischitz <thomas@xamoom.com>
2019-03-21 15:15:59 +0100

    pod install

--- XamoomSDK/XMMCDPushDevice.h
+++ XamoomSDK/XMMCDPushDevice.h
@@ -12,10 +12,14 @@
 @interface XMMCDPushDevice  : NSManagedObject <XMMCDResource>
 
 @property (nonatomic, copy) NSString *uid;
-@property (nonatomic, copy) NSString *spotDescription;
-@property (nonatomic, copy) NSString *image;
-@property (nonatomic) NSNumber *category;
-@property (nonatomic) NSMutableDictionary *locationDictionary;
-@property (nonatomic) NSArray *tags;
+@property (nonatomic, copy) NSString *os;
+@property (nonatomic, copy) NSString *appVersion;
+@property (nonatomic) NSString *appId;
+@property (nonatomic) NSString *lastAppOpen;
+@property (nonatomic) NSString *updatedAt;
+@property (nonatomic) NSString *createdAt;
+@property (nonatomic) NSDictionary *location;
+@property (nonatomic) NSString *language;
+@property (nonatomic) NSString *sdkVersion;
 
 @end
