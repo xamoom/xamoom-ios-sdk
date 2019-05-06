e32ca8115f8ac9fd9e7d35bdabed3a8de4bdf6bd
Thomas Krainz-Mischitz <thomas@xamoom.com>
2019-05-06 12:28:47 +0200

    EnduserApi push device version to 3.11.3

--- XamoomSDK/Classes/XMMEnduserApi.m
+++ XamoomSDK/Classes/XMMEnduserApi.m
@@ -813,7 +813,7 @@ static XMMEnduserApi *sharedInstance;
   NSString *token = [storage getUserToken];
   NSString *version = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleShortVersionString"];
   NSString *appId = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleIdentifier"];
-  NSString *sdkVersion = @"3.11.1-dev";
+  NSString *sdkVersion = @"3.11.3";
 
   if (location != nil && token != nil && version != nil && appId != nil) {
     XMMPushDevice *device = [[XMMPushDevice alloc] init];
