091464cdcf447bb88d80beb139178d5e527fe92e
Thomas Krainz-Mischitz <thomas@xamoom.com>
2019-05-13 18:40:33 +0200

    version bump to 3.11.2

--- XamoomSDK/Classes/XMMEnduserApi.m
+++ XamoomSDK/Classes/XMMEnduserApi.m
@@ -815,7 +815,7 @@ static XMMEnduserApi *sharedInstance;
   NSString *token = [storage getUserToken];
   NSString *version = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleShortVersionString"];
   NSString *appId = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleIdentifier"];
-  NSString *sdkVersion = @"3.11.1";
+  NSString *sdkVersion = @"3.11.2";
   
   if (location != nil && token != nil && version != nil && appId != nil) {
     XMMPushDevice *device = [[XMMPushDevice alloc] init];
