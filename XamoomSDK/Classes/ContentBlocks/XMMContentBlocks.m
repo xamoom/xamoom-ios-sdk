c7bbae4882b3f7bfa36d8d4a7b931b65efcc20af
Thomas Krainz-Mischitz <thomas@xamoom.com>
2019-10-01 11:40:10 +0200

    fix issue in XMMContentBlocks

--- XamoomSDK/Classes/ContentBlocks/XMMContentBlocks.m
+++ XamoomSDK/Classes/ContentBlocks/XMMContentBlocks.m
@@ -313,7 +313,6 @@ NSString* const kContentBlock9MapContentLinkNotification = @"com.xamoom.ios.kCon
   }
   
   XMMContentBlock *block = [self.items objectAtIndex:indexPath.row];
-  block.contentID = self.content.ID;
   if (block.blockType == -2) {
     XMMContentBlockEventTableViewCell *eventCell = [tableView dequeueReusableCellWithIdentifier:@"XMMContentBlockEventTableViewCell" forIndexPath:indexPath];
     [eventCell setNavigationType:self.navigationType];
