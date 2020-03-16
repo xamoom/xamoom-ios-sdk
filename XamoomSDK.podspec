c485dab551a8a37fe72cf8106f60c84009fbd571
Bruno <bruno@xamoom.com>
2020-03-16 13:56:46 +0100

    v3.11.18

--- XamoomSDK.podspec
+++ XamoomSDK.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name             = "XamoomSDK"
-  s.version          = "3.11.17"
+  s.version          = "3.11.18"
   s.summary          = "Integrate your app with your xamoom system. More information at www.xamoom.com"
   s.homepage         = "http://xamoom.github.io/xamoom-ios-sdk/"
   s.license          = { :type => 'MIT', :file => 'LICENSE' }
--- XamoomSDK/Classes/XMMEnduserApi.m
+++ XamoomSDK/Classes/XMMEnduserApi.m
@@ -833,7 +833,7 @@ static XMMEnduserApi *sharedInstance;
   NSString *token = [storage getUserToken];
   NSString *version = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleShortVersionString"];
   NSString *appId = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleIdentifier"];
-  NSString *sdkVersion = @"3.11.17";
+  NSString *sdkVersion = @"3.11.18";
   
   if (token != nil && version != nil && appId != nil) {
     XMMPushDevice *device = [[XMMPushDevice alloc] init];
