474ea03b63ebc5aa3167eeb69fb15a4b9e19feab
Petar Cekic <petar@xamoom.com>
2020-08-20 14:45:17 +0200

    v3.11.20

--- XamoomSDK/Classes/XMMEnduserApi.m
+++ XamoomSDK/Classes/XMMEnduserApi.m
@@ -886,7 +886,7 @@ static XMMEnduserApi *sharedInstance;
   NSString *token = [storage getUserToken];
   NSString *version = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleShortVersionString"];
   NSString *appId = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleIdentifier"];
-  NSString *sdkVersion = @"3.11.19";
+  NSString *sdkVersion = @"3.11.20";
   
   if (token != nil && version != nil && appId != nil) {
     XMMPushDevice *device = [[XMMPushDevice alloc] init];
