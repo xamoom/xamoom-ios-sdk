6eef7029dbd90319d59181b746853d95e21676f8
G0yter <goyter012@gmail.com>
2020-10-29 18:40:10 +0200

    ol and ul displaying improved

--- XamoomSDK/Classes/ContentBlocks/Blocks/XMMContentBlock0TableViewCell.m
+++ XamoomSDK/Classes/ContentBlocks/Blocks/XMMContentBlock0TableViewCell.m
@@ -103,52 +103,95 @@ static UIColor *contentLinkColor;
 - (NSMutableAttributedString*)attributedStringFromHTML:(NSString*)html fontSize:(int)fontSize fontColor:(UIColor *)color {
   NSError *err = nil;
   
-  NSString *style = [NSString stringWithFormat:@"<style>body{font-family: \"Helvetica Neue Light\", \"Helvetica Neue\", Helvetica, Arial, \"Lucida Grande\", sans-serif; font-size:%d; margin:0 !important;} p:last-child{text-align:right;}, p:last-of-type{margin:0px !important;} </style>", fontSize];
   
-  html = [html stringByReplacingOccurrencesOfString:@"<p></p>" withString:@""];
-  html = [html stringByReplacingOccurrencesOfString:@"</ul>" withString:@"</ul><br>"];
-  html = [html stringByReplacingOccurrencesOfString:@"</p>" withString:@"</p><br>"];
-  html = [NSString stringWithFormat:@"%@%@", style, html];
-  
-  if ([[html substringFromIndex:[html length] - 12] isEqualToString:@"<br></p><br>"]) {
-    html = [html substringToIndex:[html length] - 12];
-    html = [html stringByAppendingString:@"</p>"];
-  } else if ([[html substringFromIndex:[html length] - 4] isEqualToString:@"<br>"] ) {
-    html = [html substringToIndex:[html length] - 4];
-    html = [html stringByAppendingString:@"</p>"];
-  } else if ([[html substringFromIndex:[html length] - 8] isEqualToString:@"</p><br>"] || [[html substringFromIndex:[html length] - 8] isEqualToString:@"<br></p>"]) {
-    html = [html substringToIndex:[html length] - 8];
-    html = [html stringByAppendingString:@"</p>"];
-  }
-  
-  html = [html stringByReplacingOccurrencesOfString:@"</p></p>" withString:@"</p>"];
-  //html = [html stringByReplacingOccurrencesOfString:@"</p><br><p>" withString:@""];
-  
-  NSMutableAttributedString *attributedString =
-  [[NSMutableAttributedString alloc] initWithData: [html dataUsingEncoding:NSUTF8StringEncoding]
-                                          options: @{ NSDocumentTypeDocumentAttribute: NSHTMLTextDocumentType,
-                                                      NSCharacterEncodingDocumentAttribute: @(NSUTF8StringEncoding)}
-                               documentAttributes: nil
-                                            error: &err];
-  
-  if(err) {
-    NSLog(@"Unable to parse label text: %@", err);
-  }
-  
-  NSMutableParagraphStyle *paragraphStyle = [[NSMutableParagraphStyle alloc] init];
-  [paragraphStyle setBaseWritingDirection:NSWritingDirectionNatural];
+    
+    NSMutableAttributedString *attributedString =
+    [[NSMutableAttributedString alloc] initWithData: [@"" dataUsingEncoding:NSUTF8StringEncoding]
+                                            options: @{ NSDocumentTypeDocumentAttribute: NSHTMLTextDocumentType,
+                                                        NSCharacterEncodingDocumentAttribute: @(NSUTF8StringEncoding)}
+                                 documentAttributes: nil
+                                              error: &err];
+    
+    
+    if([html containsString:@"<ul>"] || [html containsString:@"<ol>"]) {
+        NSMutableArray* splitedText = [NSMutableArray new];
+        if([html containsString:@"<ul>"] && !([html containsString:@"<ol>"])){
+            [splitedText addObjectsFromArray:[self getArrayOfHtmlStrings:html fontSize:fontSize listType:@"<ul>"]];
+        } else if ([html containsString:@"<ol>"] && !([html containsString:@"<ul>"])) {
+            [splitedText addObjectsFromArray:[self getArrayOfHtmlStrings:html fontSize:fontSize listType:@"<ol>"]];
+        } else {
+            [splitedText addObjectsFromArray:[self getArrayOfHtmlStrings:html fontSize:fontSize listType:@"<ul>"]];
+        }
+//        NSArray* splitedText = [self getArrayOfHtmlStrings:html fontSize:fontSize];
+        for (NSString *textPart in splitedText){
+            NSString *modifiedTextPart = [self getStyledHtmlString:textPart fontSize:fontSize];
+                if([modifiedTextPart containsString:@"<ul>"] || [modifiedTextPart containsString:@"<ol>"]) {
+                    NSMutableAttributedString *listAttributedString = [[NSMutableAttributedString alloc] initWithData: [modifiedTextPart dataUsingEncoding:NSUTF8StringEncoding]
+                           options: @{ NSDocumentTypeDocumentAttribute: NSHTMLTextDocumentType,
+                                       NSCharacterEncodingDocumentAttribute: @(NSUTF8StringEncoding)}
+                documentAttributes: nil
+                             error: &err];
+                
+                    NSMutableParagraphStyle *paragraphStyle = [[NSMutableParagraphStyle alloc] init];
+                    [paragraphStyle setBaseWritingDirection:NSWritingDirectionNatural];
 
-  if ([html containsString:@"<ul><li>"]) {
-    [paragraphStyle addTabStop:[[NSTextTab alloc] initWithTextAlignment:NSTextAlignmentLeft location:10 options:nil]];
-    [paragraphStyle setDefaultTabInterval:10];
-    [paragraphStyle setHeadIndent:28];
-    [paragraphStyle setParagraphSpacing:5];
+                    if (@available(iOS 9.0, *)) {
+                        [paragraphStyle addTabStop:[[NSTextTab alloc] initWithTextAlignment:NSTextAlignmentLeft location:10 options:nil]];
+                    }
+                    [paragraphStyle setHeadIndent:28];
+                    [paragraphStyle setParagraphSpacing:5];
+                    [paragraphStyle setAlignment:NSTextAlignmentNatural];
+                
+                    [listAttributedString addAttribute:NSParagraphStyleAttributeName
+                                         value:paragraphStyle
+                                         range:NSMakeRange(0, [listAttributedString length])];
+                
+                    [attributedString appendAttributedString:listAttributedString];
+                    
+                
+                } else {
+                    NSMutableAttributedString *ordinaryAttributedStringNearList = [[NSMutableAttributedString alloc] initWithData: [modifiedTextPart dataUsingEncoding:NSUTF8StringEncoding]
+                           options: @{ NSDocumentTypeDocumentAttribute: NSHTMLTextDocumentType,
+                                       NSCharacterEncodingDocumentAttribute: @(NSUTF8StringEncoding)}
+                documentAttributes: nil
+                             error: &err];
+                
+                    NSMutableParagraphStyle *ordinaryParagraphStyleNearList = [[NSMutableParagraphStyle alloc] init];
+                    [ordinaryParagraphStyleNearList setBaseWritingDirection:NSWritingDirectionNatural];
+
+                    [ordinaryAttributedStringNearList addAttribute:NSParagraphStyleAttributeName
+                                         value:ordinaryParagraphStyleNearList
+                                         range:NSMakeRange(0, [ordinaryAttributedStringNearList length])];
+                
+                    [attributedString appendAttributedString:ordinaryAttributedStringNearList];
+            }
+        }
+            
+    } else {
+       NSString *modifiedTextPartOrdinary = [self getStyledHtmlString:html fontSize:fontSize];
+       NSMutableAttributedString *ordinaryAttributedString = [[NSMutableAttributedString alloc] initWithData: [modifiedTextPartOrdinary dataUsingEncoding:NSUTF8StringEncoding]
+                   options: @{ NSDocumentTypeDocumentAttribute: NSHTMLTextDocumentType,
+                               NSCharacterEncodingDocumentAttribute: @(NSUTF8StringEncoding)}
+        documentAttributes: nil
+                     error: &err];
+        
+        NSMutableParagraphStyle *ordinaryParagraphStyle = [[NSMutableParagraphStyle alloc] init];
+        [ordinaryParagraphStyle setBaseWritingDirection:NSWritingDirectionNatural];
+
+        [ordinaryAttributedString addAttribute:NSParagraphStyleAttributeName
+                                 value:ordinaryParagraphStyle
+                                 range:NSMakeRange(0, [ordinaryAttributedString length])];
+        
+        [attributedString appendAttributedString:ordinaryAttributedString];
+    }
+    
+    
+
+
+  if(err) {
+      NSLog(@"Unable to parse label text: %@", err);
   }
-  
-  [attributedString addAttribute:NSParagraphStyleAttributeName
-                           value:paragraphStyle
-                           range:NSMakeRange(0, [attributedString length])];
-  
+    
   if (color != nil) {
     [attributedString addAttribute:NSForegroundColorAttributeName
                              value:color
@@ -161,6 +204,89 @@ static UIColor *contentLinkColor;
   }
   
   return attributedString;
+}
+
+-(NSString*) getStyledHtmlString: (NSString*)html fontSize:(int)fontSize {
+    NSString *style = [NSString stringWithFormat:@"<style>body{font-family: \"Helvetica Neue Light\", \"Helvetica Neue\", Helvetica, Arial, \"Lucida Grande\", sans-serif; font-size:%d; margin:0 !important;} p:last-child{text-align:right;}, p:last-of-type{margin:0px !important;} </style>", fontSize];
+    
+    html = [html stringByReplacingOccurrencesOfString:@"<p></p>" withString:@""];
+    html = [html stringByReplacingOccurrencesOfString:@"</ul>" withString:@"</ul><br>"];
+    html = [html stringByReplacingOccurrencesOfString:@"</p>" withString:@"</p><br>"];
+    html = [NSString stringWithFormat:@"%@%@", style, html];
+    
+    if(!([html containsString:@"<ul>"])) {
+        if ([[html substringFromIndex:[html length] - 12] isEqualToString:@"<br></p><br>"]) {
+            html = [html substringToIndex:[html length] - 12];
+            html = [html stringByAppendingString:@"</p>"];
+        } else if ([[html substringFromIndex:[html length] - 4] isEqualToString:@"<br>"] ) {
+            html = [html substringToIndex:[html length] - 4];
+        html = [html stringByAppendingString:@"</p>"];
+        } else if ([[html substringFromIndex:[html length] - 8] isEqualToString:@"</p><br>"] ||     [[html substringFromIndex:[html length] - 8] isEqualToString:@"<br></p>"]) {
+        html = [html substringToIndex:[html length] - 8];
+        html = [html stringByAppendingString:@"</p>"];
+        }
+    }
+    
+    html = [html stringByReplacingOccurrencesOfString:@"</p></p>" withString:@"</p>"];
+    return html;
+}
+
+- (NSString *) getEndTag: (NSString *) tag{
+    NSMutableString *endTag = [NSMutableString stringWithString:tag];
+    [endTag insertString:@"/" atIndex:1];
+    return [NSString stringWithString:endTag];
+}
+
+
+- (NSMutableArray *) getArrayOfHtmlStrings: (NSString *) text fontSize: (int)fontSize listType: (NSString *) listType {
+    NSMutableArray *result = [NSMutableArray array];
+    
+    NSString *pattern = [NSString stringWithFormat:@"%@((.|\n|\r)*)%@", listType, [self getEndTag:listType]];
+    
+    NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:pattern options:NSRegularExpressionCaseInsensitive error:nil];
+    NSArray *matches = [regex matchesInString:text
+    options:0
+      range:NSMakeRange(0, [text length])];
+    
+    
+    for(int i = 0; i < [matches count]; i++) {
+        NSRange currentMatchRange = [[matches objectAtIndex:i] range];
+        int thisListStartIndex = currentMatchRange.location;
+        int thisListEndIndex = currentMatchRange.location + currentMatchRange.length;
+        NSRange nextMatchRange;
+        int nextListStartIndex;
+        int nextListEndIndex;
+        if(i != ([matches count] -1)) {
+            nextMatchRange = [[matches objectAtIndex:i+1] range];
+            nextListStartIndex = nextMatchRange.location;
+            nextListEndIndex = nextMatchRange.location + nextMatchRange.length;
+        }
+        
+    
+        //adding first before <ul> element
+        if(i == 0 && thisListStartIndex != 0) {
+            NSString *headString = [text substringWithRange:NSMakeRange(0, thisListStartIndex)];
+            [result addObject:[self getStyledHtmlString:headString fontSize:fontSize]];
+        }
+        
+        //adding <ul> element
+        NSString *listString = [text substringWithRange:NSMakeRange(thisListStartIndex, thisListEndIndex - thisListStartIndex)];
+        [result addObject:[self getStyledHtmlString:listString fontSize:fontSize]];
+        
+        //adding string after <ul> element
+        if(i == ([matches count] - 1) && thisListEndIndex != text.length) {
+            NSString *tailString = [text substringWithRange:NSMakeRange(thisListEndIndex, text.length - thisListEndIndex)];
+            [result addObject:[self getStyledHtmlString:tailString fontSize:fontSize]];
+        } else if(i != ([matches count] - 1) && thisListEndIndex != nextListStartIndex) {
+            
+                NSString *stringAfterListString = [text substringWithRange:NSMakeRange(thisListEndIndex, nextListStartIndex - thisListEndIndex)];
+                [result addObject:[self getStyledHtmlString:stringAfterListString fontSize:fontSize]];
+        
+        }
+    }
+        
+    return result;
+    
 }
 
 @end
