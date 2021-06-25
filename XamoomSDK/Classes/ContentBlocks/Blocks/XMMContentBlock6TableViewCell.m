bc9e9fcecd333df3f7fb2ba38fb2f9402d9458be
G0yter <goyter012@gmail.com>
2021-06-25 18:55:06 +0300

    cb content modifications

--- XamoomSDK/Classes/ContentBlocks/Blocks/XMMContentBlock6TableViewCell.m
+++ XamoomSDK/Classes/ContentBlocks/Blocks/XMMContentBlock6TableViewCell.m
@@ -73,16 +73,28 @@ static NSString *language;
           return;
         }
         
-        [self.loadingIndicator stopAnimating];
-        
-        [[XMMContentBlockListsCache sharedInstance] saveContent:content key:content.ID];
-        [[XMMContentBlocksCache sharedInstance] saveContent:content key:content.ID];
-        [self showBlockData:content];
+        //One more request if content property 'relatedSpot.name' is not set
+        XMMSpot *currentRelatedSpot = content.relatedSpot;
+        if (currentRelatedSpot != nil && currentRelatedSpot.ID != nil && currentRelatedSpot.name == nil) {
+            [api spotWithID:currentRelatedSpot.ID completion:^(XMMSpot *spot, NSError *error) {
+                content.relatedSpot = spot;
+                [self finishContentLoading:content];
+            }];
+        } else {
+            [self finishContentLoading:content];
+        }
       }];
   } else {
       [self showBlockData:cachedContent];
   }
   
+}
+
+- (void)finishContentLoading:(XMMContent *)content {
+    [self.loadingIndicator stopAnimating];
+    [[XMMContentBlockListsCache sharedInstance] saveContent:content key:content.ID];
+    [[XMMContentBlocksCache sharedInstance] saveContent:content key:content.ID];
+    [self showBlockData:content];
 }
 
 - (void)showBlockData:(XMMContent *)content {
@@ -91,6 +103,68 @@ static NSString *language;
   //set title and excerpt
   self.contentTitleLabel.text = self.content.title;
   self.contentExcerptLabel.text = self.content.contentDescription;
+  NSDate *contentFromDate = self.content.fromDate;
+  NSString *contentLocationName = self.content.relatedSpot.name;
+  //set time and lcoation
+  if (contentFromDate != nil || (self.content.relatedSpot != nil && contentLocationName != nil)) {
+      //prepare description text to use in 1 line
+      NSMutableAttributedString *descText = [[NSMutableAttributedString alloc] initWithString:self.content.contentDescription];
+      
+      //get label max char in line
+      int maxCharsInLabel = [self maxCharInLine] * 2;
+      if (contentFromDate != nil && contentLocationName != nil) {
+          maxCharsInLabel = [self maxCharInLine];
+      } else if (descText.length < maxCharsInLabel) {
+          maxCharsInLabel = descText.length;
+      }
+      
+      //substring description text to 1 or 2 lines based on maxCharSiseInLine
+      descText = [descText attributedSubstringFromRange:NSMakeRange(0, maxCharsInLabel)];
+      
+      //configure contentExcerptLabel text
+      NSMutableAttributedString *newLine= [[NSMutableAttributedString alloc] initWithString:@"\n"];
+      NSMutableAttributedString *descriptionString= [[NSMutableAttributedString alloc] initWithAttributedString:descText];
+      
+      if (contentFromDate != nil) {
+          //prepare event dateTime
+          NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+          NSLocale *locale = [NSLocale currentLocale];
+          [dateFormatter setLocale:locale];
+          [dateFormatter setDateFormat:@"E, d MMM, HH:mm"];
+          NSString *eventDateString = [dateFormatter stringFromDate:contentFromDate];
+          NSString *eventDate = [NSString stringWithFormat:@"%@%@",@" ",eventDateString];
+          NSMutableAttributedString *eventDateAtrString= [[NSMutableAttributedString alloc] initWithString:eventDate];
+         
+          //clock icon
+          NSTextAttachment *clockAttachment = [[NSTextAttachment alloc] init];
+          clockAttachment.image = [UIImage imageNamed:@"clock"];
+          clockAttachment.bounds = CGRectMake(0, 0, clockAttachment.image.size.width * 0.75, clockAttachment.image.size.height * 0.75);
+          NSAttributedString *clockAttachmentString = [NSAttributedString attributedStringWithAttachment:clockAttachment];
+          
+          [descriptionString appendAttributedString:newLine];
+          [descriptionString appendAttributedString:clockAttachmentString];
+          [descriptionString appendAttributedString:eventDateAtrString];
+      }
+      
+      if (contentLocationName != nil) {
+          //location icon
+          NSTextAttachment *locationAttachment = [[NSTextAttachment alloc] init];
+          locationAttachment.image = [UIImage imageNamed:@"location"];
+          locationAttachment.bounds = CGRectMake(0, 0, locationAttachment.image.size.width * 0.75, locationAttachment.image.size.height * 0.75);
+          NSAttributedString *locationAttachmentString = [NSAttributedString attributedStringWithAttachment:locationAttachment];
+          
+          //location text
+          NSMutableAttributedString *locationString= [[NSMutableAttributedString alloc] initWithString:@" "];
+          [locationString appendAttributedString: [[NSMutableAttributedString alloc] initWithString:contentLocationName]];
+          
+          
+          [descriptionString appendAttributedString:newLine];
+          [descriptionString appendAttributedString:locationAttachmentString];
+          [descriptionString appendAttributedString:locationString];
+      }
+      
+      self.contentExcerptLabel.attributedText = descriptionString;
+  }
   [self.contentExcerptLabel sizeToFit];
   
   if (self.content.imagePublicUrl == nil) {
@@ -104,7 +178,7 @@ static NSString *language;
         if (error) {
           [self setNoImageConstraints];
         }
-      }]; 
+      }];
     } else {
       [self.contentImageView sd_setImageWithURL: [NSURL URLWithString: self.content.imagePublicUrl]];
     }
@@ -114,6 +188,17 @@ static NSString *language;
 - (void)setNoImageConstraints {
   self.contentImageWidthConstraint.constant = 0;
   self.contentTitleLeadingConstraint.constant = 0;
+}
+
+- (int)maxCharInLine {
+    // calculate width for ten symbols
+    CGSize expectedLabelSizeForTenChars = [@"Musikevent" sizeWithFont:self.contentExcerptLabel.font
+                            constrainedToSize:CGSizeMake(10000, self.contentExcerptLabel.bounds.size.height)
+                            lineBreakMode:self.contentExcerptLabel.lineBreakMode];
+    float singleCharWidth = expectedLabelSizeForTenChars.width / 10;
+    
+    int maxCharInLine = (self.contentExcerptLabel.bounds.size.width / singleCharWidth) - 2;
+    return maxCharInLine;
 }
 
 @end
